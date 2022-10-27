6178d9d0ac247958fa9f350c029524d7fa734629
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2022-10-27 19:11:20 +0100

    feat: avatar ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/App.css';
+import Avatar from './components/Avatar';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>heyy</main>
+      <main>
+        <Avatar />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Avatar/index.js
@@ -0,0 +1,12 @@
+import avatar from '../../assets/avatar.jpg';
+import './styles.css';
+
+const Avatar = () => {
+  return (
+    <div className="avatar-container">
+      <img src={avatar} alt="avatar" />
+    </div>
+  );
+};
+
+export default Avatar;
